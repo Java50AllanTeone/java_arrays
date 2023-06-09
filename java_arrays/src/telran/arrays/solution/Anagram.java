b68e447b238564d2778b4d33d86b545f2cab75ca
allan <allanteone@gmail.com>
2023-06-09 18:19:46 +0300

    adds isAnagram and tests

---
+++ java_arrays/src/telran/arrays/solution/Anagram.java
@@ -0,0 +1,43 @@
+package telran.arrays.solution;
+
+public class Anagram {
+	static int INTERVAL = 'a' - 'A';
+	
+	public static boolean isAnagram(String sourceWord, String targetWord) {
+		byte[] helper = new byte['z' + 1];
+		boolean res = true;
+		int index = 0;
+		
+		if (sourceWord.length() == 0 || sourceWord.length() - targetWord.length() != 0)
+			return false;
+		
+		while (index < sourceWord.length() && res) {
+			char sourceCh = toLowerCh(sourceWord.charAt(index));
+			char targetCh = toLowerCh(targetWord.charAt(index));
+			
+			if (isValidSymb(sourceCh) && isValidSymb(targetCh)) {
+				helper[sourceCh]++;
+				helper[targetCh]--;
+			} else {
+				res = false;
+			}
+			index++;
+		}
+		
+		for (int i = 0; i < helper.length; i++) {
+			if (helper[i] != 0) {
+				res = false;
+			}
+		}
+		return res;
+	}
+	
+	public static boolean isValidSymb(char ch) {
+		return ch >= 97 && ch <= 122;
+	}
+	
+	public static char toLowerCh(char ch) {
+		return (char) (ch < 97 ? ch + INTERVAL : ch);
+	}
+
+}
---
+++ java_arrays/src/telran/arrays/test/AnagramTest.java
@@ -0,0 +1,82 @@
+package telran.arrays.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.arrays.solution.Anagram;
+
+class AnagramTest {
+
+	@Test
+	void isAnagramTest() {
+		//pos
+		String target = "abcde";
+		String src = "abcde";
+		assertTrue(Anagram.isAnagram(src, target));
+		
+		target = "abcde";
+		src = "cbaed";
+		assertTrue(Anagram.isAnagram(src, target));
+		
+		target = "AbcdE";
+		src = "cbaed";
+		assertTrue(Anagram.isAnagram(src, target));
+		
+		target = "abcde";
+		src = "AbcdE";
+		assertTrue(Anagram.isAnagram(src, target));
+		
+		target = "aBCDe";
+		src = "AbcdE";
+		assertTrue(Anagram.isAnagram(src, target));
+		
+		//neg
+		
+		target = "abcde";
+		src = "abcd";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+		target = "abcd";
+		src = "abcde";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+		target = "";
+		src = "abcde";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+		target = "abcde";
+		src = "";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+		target = "";
+		src = "";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+		target = "ab1cde";
+		src = "abcde";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+		target = "abcde";
+		src = "ab1cde";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+		target = "ab1cde";
+		src = "ab1cde";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+		target = "ab~cde";
+		src = "abcde";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+		target = "abcde";
+		src = "ab~cde";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+		target = "ab~cde";
+		src = "ab~cde";
+		assertFalse(Anagram.isAnagram(src, target));
+		
+	}
+
+}
